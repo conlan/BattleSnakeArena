03542b79e2f1f7129f967030984414bdf5493450
Conlan Rios <conlan@gmail.com>
2024-01-19 14:44:28 -0500

    Put reporting behind training flag

--- battlesnake.py
+++ battlesnake.py
@@ -621,20 +621,20 @@ def main():
 
         turns = game_results["turns"]
         running_turns_count.append(turns)
-
-        running_training_losses.extend(game_results["training_losses"])
-
         
-        print(f'{i+1} / {args.games}) Turn Mean: {sum(running_turns_count) * 1.0 / len(running_turns_count):.3f}')    
+        if (args.train_reinforcement):
+            print(f'{i+1} / {args.games}) Turn Mean: {sum(running_turns_count) * 1.0 / len(running_turns_count):.3f}')
 
-        if (args.discord_webhook_url):
-            if (i + 1) % REPORT_TO_DISCORD_EVERY == 0:                
-                rl_utils.report_to_discord(args.discord_webhook_url[0], {
-                    "running_turns_count" : running_turns_count,
-                    "running_training_losses" : running_training_losses,
-                    "winners" : winners,
-                    "training_snake_name" : training_snake_name                    
-                })
+            running_training_losses.extend(game_results["training_losses"])
+        
+            if (args.discord_webhook_url):
+                if (i + 1) % REPORT_TO_DISCORD_EVERY == 0:                
+                    rl_utils.report_to_discord(args.discord_webhook_url[0], {
+                        "running_turns_count" : running_turns_count,
+                        "running_training_losses" : running_training_losses,
+                        "winners" : winners,
+                        "training_snake_name" : training_snake_name                    
+                    })
 
     for winner in set(winners):
         if (winner == GAME_RESULT_DRAW):

796c801cb7d43d7b3fe0e04d8ed5058577080ab2
Conlan Rios <conlan@gmail.com>
2024-02-19 17:18:08 -0500

    Remove snapshotted snake as we're going to learn again

--- barracks.py
+++ barracks.py
@@ -39,13 +39,13 @@ def main(model_save_path, history_save_path, discord_webhook_url) -> None:
     # Simple Controller
     training_opponent_0 = SimpleController("sc")
     # Snapshotted DDQN Controller
-    training_opponent_1 = DDQNController("_________", "v10", convert_data_to_image=observer.convert_data_to_image)
-    training_opponent_1.load_epsilon(constants.EPSILON_INFO_ALWAYS_GREEDY)
+    # training_opponent_1 = DDQNController("_________", "v10", convert_data_to_image=observer.convert_data_to_image)
+    # training_opponent_1.load_epsilon(constants.EPSILON_INFO_ALWAYS_GREEDY)
     # ========================================================================
 
     training_opponents = [
         training_opponent_0
-        ,training_opponent_1
+        # ,training_opponent_1
     ]
 
     training_config = {

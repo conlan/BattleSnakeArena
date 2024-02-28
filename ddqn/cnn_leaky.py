24462e2ddb60d6db0f84e7713abc206e456d32d4
Conlan Rios <conlan@gmail.com>
2024-02-28 00:00:21 -0500

    On second thought let's not go to batch normalization

--- ddqn/cnn_leaky.py
+++ ddqn/cnn_leaky.py
@@ -7,16 +7,12 @@ class CNNLeaky(nn.Module):
 
         # 11 x 11 -> 10 x 10
         self.conv1 = nn.Conv2d(in_channels=13, out_channels=32, kernel_size=2)
-        self.batch_norm1 = nn.BatchNorm2d(32)
         # 10 x 10 -> 9 x 9
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2)
-        self.batch_norm2 = nn.BatchNorm2d(64)
         # 9 x 9 -> 8 x 8
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2)
-        self.batch_norm3 = nn.BatchNorm2d(128)
         # 8 x 8 -> 8 x 8
         self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1)
-        self.batch_norm4 = nn.BatchNorm2d(256)
         # 256 * 7 * 7 -> 2304
         self.fc1 = nn.Linear(16384, 512)
 
@@ -49,17 +45,14 @@ class CNNLeaky(nn.Module):
         # self.fc2 = nn.Linear(512, output_size)
 
     def forward(self, x):
-        if (len(x.shape) == 3):
-            x = x.unsqueeze(0)
-
         # 11 x 11 -> 9 x 9
-        x = F.leaky_relu(self.batch_norm1(self.conv1(x)))
+        x = F.leaky_relu(self.conv1(x))
         # 9 x 9 -> 7 x 7
-        x = F.leaky_relu(self.batch_norm2(self.conv2(x)))
+        x = F.leaky_relu(self.conv2(x))
         # 7 x 7 -> 5 x 5
-        x = F.leaky_relu(self.batch_norm3(self.conv3(x)))
+        x = F.leaky_relu(self.conv3(x))
         # 5 x 5 -> 3 x 3
-        x = F.leaky_relu(self.batch_norm4(self.conv4(x)))
+        x = F.leaky_relu(self.conv4(x))
         # # 256 * 3 * 3 -> 2304
         # x = x.view(-1, 2304)
         # 256 * 11 * 11 -> 30976
